072abc9ab158cc638f079315b4976acd00f29b09
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-05-10 01:31:26 +0200

    multiple quotes and clipboard

--- main.js
+++ main.js
@@ -38,7 +38,15 @@ const formatBlocks = (blocks) => {
 
 const quoteTemplate = (quote) => {
   return `
-    <div class="quote p-4 m-4">${quote}</div>
+    <div class="quote p-4 m-4">
+      <span class="quote-container">${quote}</span>
+      <div class="clipboard flex flex-row mt-4 justify-end items-center text-gray-400 text-sm hover:text-gray-500 cursor-pointer transition-all duration-300 ease-in-out">
+      <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+      </svg>
+      copy to clipboard
+      </div>
+    </div>
   `;
 };
 
@@ -63,9 +71,18 @@ const getQuotes = (element, numberOfQuotes, source) => {
   }
 };
 
+const toClipboard = (quote) => {
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state == "granted" || result.state == "prompt") {
+      navigator.clipboard.writeText(quote);
+    }
+  });
+};
+
 $(function () {
   const quotesContainer = document.querySelector(".quotes-container");
   const randomizeButton = document.querySelector(".randomize-button");
+  const validateButton = document.querySelector(".validate-button");
   const rangeValue = document.querySelector("#rangevalue");
   const rangeInput = document.querySelector(".range");
   const source = document.querySelector(".collections-select");
@@ -75,12 +92,35 @@ $(function () {
   rangeValue.value = numberOfQuotes;
 
   rangeInput.addEventListener("input", () => {
+    randomizeButton.style.display = "flex";
     rangeValue.value = rangeInput.value;
     numberOfQuotes = rangeInput.value;
+    getQuotes(quotesContainer, numberOfQuotes, source);
   });
 
   source.addEventListener("input", () => {
+    randomizeButton.style.display = "flex";
     getQuotes(quotesContainer, numberOfQuotes, source);
+  });
+
+  validateButton.addEventListener("click", () => {
+    randomizeButton.style.display = "none";
+    const clipboards = document.querySelectorAll(".clipboard");
+    const quote = document.querySelectorAll(".quote");
+    const quoteContent = document.querySelectorAll(".quote-container");
+
+    for (let el of clipboards) {
+      el.classList.add("visible");
+    }
+    for (let el of quote) {
+      el.classList.add("isDone");
+    }
+
+    clipboards.forEach((quote, index) => {
+      quote.addEventListener("click", () => {
+        toClipboard(quoteContent[index].innerHTML);
+      });
+    });
   });
 
   getQuotes(quotesContainer, numberOfQuotes, source);
