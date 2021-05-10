9c4d578cb8275bd85b10fbefae009dd3c1bd720a
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-05-10 02:20:09 +0200

    Clipboard toggle

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ const quoteTemplate = (quote) => {
       <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
       </svg>
-      copy to clipboard
+      Copy to clipboard
       </div>
     </div>
   `;
@@ -118,6 +118,20 @@ $(function () {
 
     clipboards.forEach((quote, index) => {
       quote.addEventListener("click", () => {
+        clipboards[index].innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+        </svg>
+        Copied
+        `;
+        setTimeout(() => {
+          clipboards[index].innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+          </svg>
+          Copy to clipboard
+          `;
+        }, 2500);
         toClipboard(quoteContent[index].innerHTML);
       });
     });
