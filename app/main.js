4a9603a04956f734f9b79ca062fe9c14c2d0bd85
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-05-10 09:59:24 +0200

    Add utils

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
-import "./styles/styles.scss";
+import "../styles/styles.scss";
 import { dictionary, dictionaryAlt } from "./dictionary.js";
+import { getTemplate, toClipboard } from "./utils.js";
 
 const generateSentence = (dictionary) => {
   let blocks = [];
@@ -37,17 +38,7 @@ const formatBlocks = (blocks) => {
 };
 
 const quoteTemplate = (quote) => {
-  return `
-    <div class="quote p-4 m-4">
-      <span class="quote-container">${quote}</span>
-      <div class="clipboard flex flex-row mt-4 justify-end items-center text-gray-400 text-sm hover:text-gray-500 cursor-pointer transition-all duration-300 ease-in-out">
-      <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
-      </svg>
-      Copy to clipboard
-      </div>
-    </div>
-  `;
+  return getTemplate("quote", quote);
 };
 
 const fillQuote = (container, quote) => {
@@ -69,14 +60,6 @@ const getQuotes = (element, numberOfQuotes, source) => {
   for (let i = 0; i < numberOfQuotes; i++) {
     fillQuote(element, generateSentence(getSource(source)));
   }
-};
-
-const toClipboard = (quote) => {
-  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-    if (result.state == "granted" || result.state == "prompt") {
-      navigator.clipboard.writeText(quote);
-    }
-  });
 };
 
 $(function () {
@@ -118,24 +101,18 @@ $(function () {
 
     clipboards.forEach((quote, index) => {
       quote.addEventListener("click", () => {
-        clipboards[index].innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
-        </svg>
-        Copied
-        `;
+        clipboards[index].innerHTML = getTemplate("copy-done");
         setTimeout(() => {
-          clipboards[index].innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
-          </svg>
-          Copy to clipboard
-          `;
+          clipboards[index].innerHTML = getTemplate("copy-to-clipboard");
         }, 2500);
         toClipboard(quoteContent[index].innerHTML);
       });
     });
   });
+
+  // const savedQuotes = { ...JSON.parse(localStorage.getItem("savedQuotes")) };
+  // localStorage.setItem("savedQuotes", savedQuotes);
+  // console.log(localStorage.savedQuotes);
 
   getQuotes(quotesContainer, numberOfQuotes, source);
 
---
+++ app/utils.js
@@ -0,0 +1,40 @@
+export const getTemplate = (template, prop) => {
+  switch (template) {
+    case "quote":
+      return `
+          <div class="quote p-4 m-4">
+            <span class="quote-container">${prop}</span>
+            <div class="clipboard flex flex-row mt-4 justify-end items-center text-gray-400 text-sm hover:text-gray-500 cursor-pointer transition-all duration-300 ease-in-out">
+            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            </svg>
+            Copy to clipboard
+            </div>
+          </div>
+        `;
+    case "copy-to-clipboard":
+      return `
+          <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+          </svg>
+          Copy to clipboard
+        `;
+    case "copy-done":
+      return `
+          <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+          </svg>
+          Copied
+        `;
+    default:
+      break;
+  }
+};
+
+export const toClipboard = (quote) => {
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state == "granted" || result.state == "prompt") {
+      navigator.clipboard.writeText(quote);
+    }
+  });
+};
